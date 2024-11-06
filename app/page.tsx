5f2bf5314491389ce8a8b86ed5204c949ff25550
jhondel <jhondeldelconacaranay@gmail.com>
2024-11-06 17:34:02 +0800

    add updated banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
+import Banner from "@/components/banner/banner";
 import { Leftside } from "@/components/left-side";
 import { Navbar } from "@/components/navbar";
 import { RightSided } from "@/components/right-side";
 
 export default function Home() {
   return (
-    <div className="text-textLight h-screen max-h-screen w-full overflow-y-auto overflow-x-hidden bg-bodyColor font-bodyFont">
+    <div className="text-textLight h-screen max-h-screen w-full overflow-y-auto overflow-x-hidden bg-gradient-to-r from-bodyColor via-bodyColor to-indigo-950 font-bodyFont">
       <Navbar />
 
       <div className="h-[88vh] w-full items-center justify-between gap-20 xl:flex">
         <Leftside />
         <div className="mx-auto h-[88vh] w-full max-w-container p-4">
-          awdawd
+          <Banner />
         </div>
         <RightSided />
       </div>
---
+++ components/banner/banner.tsx
@@ -0,0 +1,136 @@
+"use client";
+
+import { motion, Variants } from "framer-motion";
+import { useEffect, useState } from "react";
+import TypewriterComponent, { Options } from "typewriter-effect";
+
+const Banner = () => {
+  const [autoStart, setAutoStart] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAutoStart(true);
+    }, 1500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <motion.div
+      id="home"
+      className="mx-auto flex max-w-contentContainer flex-col gap-4 py-10 mdl:px-10 mdl:py-24 lgl:gap-8 xl:px-4"
+      variants={bannerVariants}
+      initial="hidden"
+      animate="visible"
+    >
+      <motion.h3
+        className="font-titleFont text-lg tracking-wide text-colorGreen"
+        variants={bannerItemVariants}
+      >
+        Hi, my name is
+      </motion.h3>
+      <motion.h1
+        className="flex flex-col font-titleFont text-2xl font-semibold lg:text-4xl"
+        variants={bannerItemVariants}
+      >
+        Jhondel Delicona Caranay
+        <span>
+          <TypewriterComponent
+            options={
+              {
+                strings: [
+                  "Creating modern web experiences.",
+                  "Specializing in Node.js backend development.",
+                  "Building intuitive UIs with React.",
+                  "Passionate full-stack developer.",
+                ],
+                autoStart: autoStart,
+                loop: true,
+                pauseFor: 0,
+              } as Partial<Options>
+            }
+          />
+        </span>
+      </motion.h1>
+      <motion.p
+        className="text-base font-medium text-colorDark md:max-w-[650px]"
+        variants={bannerItemVariants}
+      >
+        I am a skilled Web Developer proficient in React, Next.js, Express, and
+        TypeScript. Experienced in creating responsive, user-friendly
+        applications with a focus on clean code and effective problem-solving.
+        Seeking opportunities to contribute my expertise in a dynamic
+        development role.
+        <a href="">
+          <span className="group relative block h-7 w-fit cursor-pointer overflow-x-hidden text-colorGreen">
+            Learn more
+            <span className="absolute bottom-1 left-0 h-[1px] w-full -translate-x-full transform bg-colorGreen text-colorGreen transition-transform duration-300 ease-in-out group-hover:translate-x-0"></span>
+          </span>
+        </a>
+      </motion.p>
+      <motion.button
+        className="h-14 w-52 rounded-md border border-colorGreen font-titleFont text-sm tracking-wide text-colorGreen duration-300 hover:bg-HoverColor"
+        variants={buttonVariants}
+        whileHover="hover"
+        initial="hidden"
+        animate="visible"
+      >
+        Check out my Project!
+      </motion.button>
+    </motion.div>
+  );
+};
+
+export default Banner;
+
+const bannerVariants: Variants = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const bannerItemVariants: Variants = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
+const buttonVariants: Variants = {
+  hidden: {
+    opacity: 0,
+    scale: 0.6,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delay: 1,
+      bounce: 1,
+    },
+  },
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      duration: 0.4,
+      repeat: Infinity,
+    },
+  },
+};
